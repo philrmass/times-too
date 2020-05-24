b5e999b1fe7da7b9a8b2248eb1573dafd2c70946
Phil Mass <philrmass@gmail.com>
2020-05-23 20:35:46 -0700

    add game content

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,11 +5,55 @@ import styles from '../styles/Game.module.css';
 function Game({
   game,
 }) {
+  function buildTime() {
+    const mins = Math.floor(game.remaining / 60);
+    const secs = game.remaining - (60 * mins);
+    const secsStr = `0${secs}`.slice(-2);
+
+    return (
+      <div className={styles.time}>
+        {`${mins}:${secsStr}`}
+      </div>
+    );
+  }
+
+  function buildQuestions() {
+    return (
+      <div className={styles.questions}>
+        {game.question + 1}/{game.questions}
+      </div>
+    );
+  }
+
+  function buildProblem() {
+    return (
+      <div className={styles.problem}>
+        {`${game.first}\nx${game.second}\n---\n${game.input}`}
+      </div>
+    );
+  }
+
+  function buildMessage() {
+    if (!game.message) {
+      return null;
+    }
+
+    return (
+      <div className={styles.message}>
+        {game.message}
+      </div>
+    );
+  }
+
   return (
     <div className={styles.main}>
-      Game
-      <div>
-        {JSON.stringify(game, null, 2)}
+      <div className={styles.game}>
+        {buildTime()}
+        {buildProblem()}
+        {buildQuestions()}
+      </div>
+      <div className={styles.message}>
+        {buildMessage()}
       </div>
     </div>
   );
